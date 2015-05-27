09497e90da0dc6a1c98d7cca4346e3a5c7b9ad4c
Matt Widmann <mgwidmann@gmail.com>
2015-05-27 10:17:17 -0400

    Update configuration.

--- config/prod.exs
+++ config/prod.exs
@@ -4,22 +4,22 @@ config :phoenix_ws_proxy,
   # Proxy will wait `sleep_factor` number of times as the speed of the request
   #   For example, 1 means wait equal to the request time, 0.5 means wait half
   #   the request time. Increase this number if your server is getting overloaded.
-  sleep_factor:  1,
+  sleep_factor:  2,
   # Proxy attempts not to DOS the server by waiting a minimum number of milliseconds.
   #   Increase this number if your server load spikes up and down.
   minimum_sleep: 2500,
   # Base URL to use for making requests to prevent making requests off site
-  base_url:      "http://www.example.com/",
+  base_url:      "http://localhost:8080/",
   # The path from `base_url` in which the polled server will respond with the decrypted
   #   session id. Notice that `:token` is the same as the `encrypted_param` setting.
-  authorize_url: "/sessions/reauth/:token",
+  authorize_url: "/reauth/:token",
   # The value inside of authorize_url to replace with the encrypted key.
   encrypted_param: ":token",
   # From the response JSON data in the `authorize_url` path, where is the decrypted
   #   session id. Should be a list of keys to reach the data.
   session_id_path: ["session_id"],
   # The cookie name for where to put the `session_id_path` data when the server is polled
-  session_id_key: :_my_app_session_id
+  session_id_key: :_appointments_session
 
 # For production, we configure the host to read the PORT
 # from the system environment. Therefore, you will need
